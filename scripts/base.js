c0d9400ba02dd556e9eeb08e51d8ced63ac71f49
petermsamba <pmsamba@byupathway.edu>
2026-02-27 12:16:45 +0200

    building landing page

---
+++ scripts/base.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('#menu-button');
+const navMenu = document.querySelector('#nav-menu');
+
+menuBtn.addEventListener('click',()=>{
+  const isOpen = navMenu.classList.toggle('open');
+  
+  
+
+});
